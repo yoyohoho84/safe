49bed69163beec1ca1abe8ec8941b0f800cb2289
Kirill <yoyohoho84@gmail.com>
2020-07-23 20:40:32 +0300

    Updates

---
+++ precache-manifest.b690f92f175f0ff341bd5cc24903dede.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4a3c8f16270ca5a93c5ac7e3ca397e4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2a83ca01a03f1490cf09",
+    "url": "./static/css/2.ed87aa75.chunk.css"
+  },
+  {
+    "revision": "2a83ca01a03f1490cf09",
+    "url": "./static/js/2.a0cf41d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.a0cf41d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbc54c3b8dacbb3e19a3",
+    "url": "./static/js/main.4c48a54e.chunk.js"
+  },
+  {
+    "revision": "949148f36890b9fd2cd1",
+    "url": "./static/js/runtime-main.933f836a.js"
+  }
+]);
